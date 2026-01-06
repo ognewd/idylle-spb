8963319e87f2bd2c308e3e1ef1f7fa22e8691bd9
Dmitriis <ognew.d@gmail.com>
2026-01-06 11:53:39 +0100

    Clean up: remove Vercel/Supabase references

--- next.config.js
+++ next.config.js
@@ -13,14 +13,6 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'idylle-spb.vercel.app',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.vercel.app',
-      },
-      {
-        protocol: 'https',
         hostname: 'images.unsplash.com',
       },
       {
